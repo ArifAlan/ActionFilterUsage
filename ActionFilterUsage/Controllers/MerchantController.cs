02e038ed2b9eea7d99b6c169a7b94df39c234b5f
ArifAlan <91489739+ArifAlan@users.noreply.github.com>
2024-06-17 19:32:06 +0300

    Add project files.

---
+++ ActionFilterUsage/Controllers/MerchantController.cs
@@ -0,0 +1,27 @@
+﻿using ActionFilterUsage.ActionFilters;
+using ActionFilterUsage.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ActionFilterUsage.Controllers
+{
+    [Route("api/{merchantCode}/[controller]")]
+    [ApiController]
+    
+    public class MerchantController : BaseController
+    {
+        [HttpGet]
+        [Route("GetUsers")]
+        public IActionResult GetUsers(string merchantCode)
+        {
+            return Ok($"Users returned for Merchant {merchantCode}. Page:{Page}.PageSize:{PageSize}");
+        }
+
+        [HttpPost]
+      
+        public IActionResult UpdateMerchant(UpdateMerchantRequestModel reqModel)
+        {
+            return Ok($"Merchant updated.Name:{reqModel.Name}, MerchantCode:{reqModel.MerchantCode}");
+        }
+    }
+}
